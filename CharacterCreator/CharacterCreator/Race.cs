82a3eecbce6ebbd19088937d8697a260c1d09269
H F <jonis88@gmail.com>
2017-10-25 13:55:22 +0200

    Sentient

---
+++ CharacterCreator/CharacterCreator/Race.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterCreator
+{
+    public class Race
+    {
+        private int strength;
+        private int stamina;
+        private string name;
+
+        public int Strength
+        {
+            get
+            {
+                return strength;
+            }
+            set
+            {
+                strength = value;
+            }
+        }
+        public int Stamina
+        {
+            get
+            {
+                return stamina;
+            }
+            set
+            {
+                stamina = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        //public void SetStamina(int sta)
+        //{
+        //    stamina = sta;
+        //}
+        //public Race (int str, int sta)
+        //{
+        //    Strength = str;
+        //    Stamina = sta;
+        //}
+
+        public Race()
+        {
+        }
+
+        //public Race(int str)
+        //{
+        //    Strength = str;
+        //}
+        //public Race(string nam)
+        //{
+        //    Name = nam;
+        //}
+
+        public Race(int str, int sta, string nam)
+        {
+            Stamina = sta;
+            Name = nam;
+            Strength = str;
+
+
+        }
+
+        public string PrintRace()
+        {
+            return "You play as " + Name + "\nYour strength is " +
+                Strength + ".\nYour stamina is " + Stamina + ".";
+        }
+    }
+}
