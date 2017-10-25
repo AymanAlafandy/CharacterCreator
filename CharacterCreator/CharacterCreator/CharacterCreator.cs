91210925946815174597f9b811515c1fcc75ea4c
John Hellman <acoppero@gmail.com>
2017-10-25 12:00:45 +0200

    Lunch commit

---
+++ CharacterCreator/CharacterCreator/CharacterCreator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterCreator
+{
+    class CharacterCreator
+    {
+        public void MainMenu()
+        {
+            Sentient elf = new Sentient();
+            elf.Strength = 53;
+            elf.Stamina = 106;
+            elf.Name = "Snapp";
+
+            Sentient orc = new Sentient(200, 80, "Bill");
+            Race human = new Race(10, 10, "Deidré");
+            Console.WriteLine(orc.PrintRace());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CharacterCreator/CharacterCreator/Sentient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterCreator
+{
+    public class Sentient : Race
+    {
+        private int intelligence;
+
+        public int Intelligence
+        {
+            get { return intelligence; }
+            set { intelligence = value; }
+        }
+
+        public Sentient() { }
+        public Sentient(int str, int sta, string nam, int inte) :
+            base(str, sta, nam)
+        {
+            Intelligence = inte;
+        }
+    }
+}
